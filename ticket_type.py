86678e10e474316a093f29415f7926e6c3d5e578
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-01 21:11:52 +0000

    [feat]: ticket type

---
+++ ticket_type.py
@@ -0,0 +1,25 @@
+# ticket selling system
+
+ticket = input("""What type of ticket do you want to buy? 
+                Enter:  g for gold
+                        s for silver
+                        p for platinum
+                .....
+                """)
+
+ticket = ticket.lower()
+
+def ticket_type(self):
+    if ticket == "g":
+        ticket_type = "Gold"
+        return ticket_type
+    elif ticket == "s":
+        ticket_type = "Silver"
+        return ticket_type
+    elif ticket == "p":
+        ticket_type = "Platinum"
+        return ticket_type
+    else:
+        return "Invalid ticket type"
+
+
