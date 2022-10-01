bcef8dc6b7b9ef7a676d13d0bee4b240c4162a2a
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-01 21:10:59 +0000

    [feat]: ticket id

---
+++ ticket_id.py
@@ -0,0 +1,15 @@
+import random
+import ticket_type
+
+def get_ticket_id(ticket_type):
+    """
+    Get ticket id by ticket type
+    """
+    if ticket_type == "Gold":
+        return random.randint(100, 1000)
+    elif ticket_type == "Silver":
+        return random.randint(1000, 2000)
+    elif ticket_type == "Platinum":
+        return random.randint(2000, 3000)
+
+print(get_ticket_id("Gold"))
