27d5b33fa75bed5403c5c69af3e9e144dad407f6
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-02 10:56:30 +0000

    [feat]: ticket price

---
+++ ticket_price.py
@@ -0,0 +1,16 @@
+import ticket_type
+import ticket_id
+
+ticket_type_amount = ticket_type.ticket_type()
+
+def ticket_price():
+    if ticket_type_amount == "Gold":
+        return "$100"
+    elif ticket_type_amount == "Silver":
+            return "$75"
+    elif ticket_type_amount == "Platinum":
+        return "$50"
+    else:
+        return None
+
+print(ticket_price())
